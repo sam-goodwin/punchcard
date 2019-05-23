2c76d5e8e0cfc09b49fe1e98adc46339fd8caecc
Sam Goodwin <samgood@amazon.com>
2019-05-23 16:40:29 -0700

    Add documentation for Queue and refactor ContextualizedQueue into QueueMap

--- packages/punchcard/lib/enumerable/enumerable.ts
+++ packages/punchcard/lib/enumerable/enumerable.ts
@@ -8,9 +8,35 @@ export interface EnumerateProps {
 }
 
 export interface IEnumerable<T, C extends ClientContext, P extends EnumerateProps> {
+  /**
+   * Client context (i.e. clients to be made available at runtime).
+   */
   readonly context: C;
+  /**
+   * Enumerate batches of values.
+   *
+   * @param scope under which this construct should be created
+   * @param id of the construct
+   * @param f next transformation of a batch of records
+   * @param props optional props for configuring the consmer
+   */
   forBatch(scope: cdk.Construct, id: string, f: (values: T[], clients: Clients<C>) => Promise<any>, props?: P): lambda.Function;
+  /**
+   * Enumerate each value.
+   *
+   * If you want to use batch APIs, e.g. for calling `PutRecords` on a kinesis stream, then use
+   * `forBatch` instead.
+   *
+   * @param scope under which this construct should be created
+   * @param id of the construct
+   * @param f next transformation of a record
+   * @param props optional props for configuring the function consuming from SQS
+   */
   forEach(scope: cdk.Construct, id: string, f: (value: T, clients: Clients<C>) => Promise<any>, props?: P): lambda.Function;
+  /**
+   * Add more clients to the client context.
+   * @param context new client context
+   */
   clients<R2 extends ClientContext>(context: R2): IEnumerable<T, C & R2, P>;
 }
 
--- packages/punchcard/lib/enumerable/queue.ts
+++ packages/punchcard/lib/enumerable/queue.ts
@@ -15,14 +15,36 @@ import { Stream } from './stream';
 
 export type EnumerateQueueProps = EnumerateProps & events.SqsEventSourceProps;
 export interface IQueue<T, C extends ClientContext> extends IEnumerable<T, C, EnumerateQueueProps> {
+  /**
+   * Asynchronously processes a `SQSEvent` and yields values.
+   *
+   * @param event sqs event payload
+   * @param clients bootstrapped clients
+   */
   run(event: SQSEvent, clients: Clients<C>): AsyncIterableIterator<T[]>;
+  /**
+   * Describe a transformation of a queue's messages.
+   *
+   * **Warning**: the transformation in a map only runs when terminated, i.e. it is
+   * lazily evaluated, so you must call `forEach` or `forBatch`.
+   *
+   * @param f transformation function
+   */
   map<U>(f: (value: T, clients: Clients<C>) => Promise<U>): IQueue<U, C>;
 }
 
+/**
+ * Props for constructing a Queue.
+ *
+ * It extends the standard `sqs.QueueProps` with a `mapper` instance.
+ */
 export interface QueueProps<T> extends sqs.QueueProps {
   mapper: Mapper<T, string>;
 }
-export class Queue<T> extends sqs.Queue implements Client<Queue.Client<T>>, IEnumerable<T, {}, EnumerateQueueProps> {
+/**
+ * Represents a SQS Queue containtining messages of type, `T`, serialized with some `Codec`.
+ */
+export class Queue<T> extends sqs.Queue implements IQueue<T, {}>, Client<Queue.ConsumeAndSendClient<T>> {
   public readonly context = {};
   public readonly mapper: Mapper<T, string>;
 
@@ -31,15 +53,33 @@ export class Queue<T> extends sqs.Queue implements Client<Queue.Client<T>>, IEnu
     this.mapper = props.mapper;
   }
 
+  /**
+   * Bottom of the recursive async generator - returns the records
+   * parsed and validated out of the SQSEvent.
+   *
+   * @param event payload of SQS event
+   */
   public async *run(event: SQSEvent): AsyncIterableIterator<T[]> {
     return yield event.Records.map(record => this.mapper.read(record.body));
   }
 
+  /**
+   * Transform each value.
+   *
+   * @param f which transforms a record
+   */
   public map<U>(f: (value: T, clients: Clients<{}>) => Promise<U>): IQueue<U, {}> {
-    return new ContextualizedQueue(this, this, this.context,
+    return new QueueMap(this, this, this.context,
       (values, clients) => Promise.all(values.map(v => f(v, clients))));
   }
 
+  /**
+   * Create and send this queue's data to a Kinesis Stream.
+   *
+   * @param scope under which this construct should be created
+   * @param id of the construct
+   * @param props optional enumerate queue props
+   */
   public toStream(scope: cdk.Construct, id: string, props?: EnumerateQueueProps): Stream<T> {
     scope = new cdk.Construct(scope, id);
     const mapper = BufferMapper.wrap(this.mapper);
@@ -54,21 +94,41 @@ export class Queue<T> extends sqs.Queue implements Client<Queue.Client<T>>, IEnu
     return stream;
   }
 
+  /**
+   * Enumerate queue messages by processing batches (of up to 10) messges.
+   *
+   * `forBatch` will subscribe a Lambda Function to the SQS Queue.
+   *
+   * @param scope under which this construct should be created
+   * @param id of the construct
+   * @param f next transformation of a batch of records
+   * @param props optional props for configuring the function consuming from SQS
+   */
   public forBatch(scope: cdk.Construct, id: string, f: (values: T[], clients: Clients<{}>) => Promise<any>, props?: EnumerateQueueProps): lambda.Function {
-    return new ContextualizedQueue<T, T, {}>(this, this, this.context, v => Promise.resolve(v)).forBatch(scope, id, f, props);
+    return new QueueMap<T, T, {}>(this, this, this.context, v => Promise.resolve(v)).forBatch(scope, id, f, props);
   }
 
   public forEach(scope: cdk.Construct, id: string, f: (value: T, clients: Clients<{}>) => Promise<any>, props?: EnumerateQueueProps): lambda.Function {
-    return new ContextualizedQueue<T, T, {}>(this, this, this.context, v => Promise.resolve(v)).forEach(scope, id, f, props);
+    return new QueueMap<T, T, {}>(this, this, this.context, v => Promise.resolve(v)).forEach(scope, id, f, props);
   }
 
+  /**
+   * Add another client to the client context.
+   * @param context new client context
+   */
   public clients<C2 extends ClientContext>(context: C2): IQueue<T, C2> {
-    return new ContextualizedQueue<T, T, C2>(this, this as any, {
+    return new QueueMap<T, T, C2>(this, this as any, {
       ...this.context,
       ...context
     }, v => Promise.resolve(v));
   }
 
+  /**
+   * Get the `queueUrl` from properties and create a SQS client for this queue's data type.
+   *
+   * @param properties runtime property bag
+   * @param cache of shared runtime state
+   */
   public bootstrap(properties: PropertyBag, cache: Cache): Queue.Client<T> {
     return new Queue.Client(
       properties.get('queueUrl'),
@@ -76,22 +136,34 @@ export class Queue<T> extends sqs.Queue implements Client<Queue.Client<T>>, IEnu
       this.mapper);
   }
 
+  /**
+   * By default, the consume and send client is installed for a Queue.
+   */
   public install(target: Runtime): void {
-    this.readWrite().install(target);
+    this.consumeAndSendClient().install(target);
   }
 
-  public readWrite(): Client<Queue.Client<T>> {
+  /**
+   * A client with permission to consume and send messages.
+   */
+  public consumeAndSendClient(): Client<Queue.ConsumeAndSendClient<T>> {
     return this._client(g => {
       this.grantConsumeMessages(g);
       this.grantSendMessages(g);
     });
   }
 
-  public read(): Client<Omit<Queue.Client<T>, 'sendMessage' | 'sendMessageBatch'>> {
+  /**
+   * A client with only permission to consume messages from this Queue.
+   */
+  public consumeClient(): Client<Queue.ConsumeClient<T>> {
     return this._client(this.grantConsumeMessages.bind(this));
   }
 
-  public write(): Client<Omit<Queue.Client<T>, 'receiveMessage'>> {
+  /**
+   * A client with only permission to send messages to this Queue.
+   */
+  public sendClient(): Client<Queue.SendClient<T>> {
     return this._client(this.grantSendMessages.bind(this));
   }
 
@@ -106,24 +178,50 @@ export class Queue<T> extends sqs.Queue implements Client<Queue.Client<T>>, IEnu
   }
 }
 
-export class ContextualizedQueue<T, U, C extends ClientContext> implements IEnumerable<U, C, EnumerateQueueProps> {
+/**
+ * Describes a transformation of messages in a Queue.
+ *
+ * **Warning**: A transformation will only be evaluated if a terminal, such
+ * as `forEach` or `forBatch`, is called.
+ */
+class QueueMap<T, U, C extends ClientContext> implements IQueue<U, C> {
   constructor(
     private readonly queue: Queue<any>,
     private readonly parent: IQueue<T, C>,
     public readonly context: C,
     private readonly f: (values: T[], clients: Clients<C>) => Promise<U[]>) {}
 
+  /**
+   * Proceses each batch from the parent queue and applies this stage's transformation.
+   * @param event SQS event
+   * @param clients bootstrapped clients
+   */
   public async *run(event: SQSEvent, clients: Clients<C>): AsyncIterableIterator<U[]> {
     for await (const batch of this.parent.run(event, clients)) {
       yield await this.f(batch, clients);
     }
   }
 
+  /**
+   * Transform each value.
+   *
+   * @param f which transforms a record
+   */
   public map<V>(f: (value: U, clients: Clients<C>) => Promise<V>): IQueue<V, C> {
-    return new ContextualizedQueue(this.queue, this, this.context,
+    return new QueueMap(this.queue, this, this.context,
       (values, clients) => Promise.all(values.map(v => f(v, clients))));
   }
 
+  /**
+   * Enumerate bathches of messages yielded by this transformation.
+   *
+   * `forBatch` will subscribe a Lambda Function to the SQS Queue.
+   *
+   * @param scope under which this construct should be created
+   * @param id of the construct
+   * @param f next transformation of a batch of records
+   * @param props optional props for configuring the function consuming from SQS
+   */
   public forBatch(scope: cdk.Construct, id: string, f: (values: U[], clients: Clients<C>) => Promise<any>, props?: EnumerateQueueProps): lambda.Function {
     props = props || {};
     const executorService = props.executorService || new LambdaExecutorService({
@@ -141,19 +239,43 @@ export class ContextualizedQueue<T, U, C extends ClientContext> implements IEnum
     return lambdaFn;
   }
 
+  /**
+   * Enumearte each record yielded by a transformation of messages in this queue.
+   *
+   * `forEach` will subscribe a Lambda Function to the SQS Queue.
+   *
+   * If you want to use batch APIs, e.g. for putting to a kinesis stream, then use
+   * `forBatch` instead.
+   *
+   * @param scope under which this construct should be created
+   * @param id of the construct
+   * @param f next transformation of a record
+   * @param props optional props for configuring the function consuming from SQS
+   */
   public forEach(scope: cdk.Construct, id: string, f: (value: U, clients: Clients<C>) => Promise<any>, props?: EnumerateQueueProps): lambda.Function {
     return this.forBatch(scope, id, (values, clients) => Promise.all(values.map(v => f(v, clients))), props);
   }
 
+  /**
+   * Add more clients to the client context.
+   * @param context new client context
+   */
   public clients<C2 extends ClientContext>(context: C2): IQueue<U, C & C2> {
-    return new ContextualizedQueue(this.queue, this.parent as any, {
+    return new QueueMap(this.queue, this.parent as any, {
       ...this.context,
       ...context
     }, this.f);
   }
 }
 
+/**
+ * Namespace for `Queue` type aliases and its `Client` implementation.
+ */
 export namespace Queue {
+  export type ConsumeAndSendClient<T> = Client<T>;
+  export type ConsumeClient<T> = Omit<Client<T>, 'sendMessage' | 'sendMessageBatch'>;
+  export type SendClient<T> = Omit<Client<T>, 'receiveMessage'>;
+
   export type ReceiveMessageRequest = Omit<AWS.SQS.ReceiveMessageRequest, 'QueueUrl'>;
   export type ReceiveMessageResult<T> = Array<{Body: T} & Omit<AWS.SQS.Message, 'Body'>>;
   export type SendMessageRequest<T> = {MessageBody: T} & Omit<AWS.SQS.SendMessageRequest, 'QueueUrl' | 'MessageBody'>;
@@ -161,6 +283,9 @@ export namespace Queue {
   export type SendMessageBatchRequest<T> = Array<{MessageBody: T} & Omit<AWS.SQS.SendMessageBatchRequestEntry, 'MessageBody'>>;
   export type SendMessageBatchResult = AWS.SQS.SendMessageBatchResult;
 
+  /**
+   * Runtime representation of a SQS Queue.
+   */
   export class Client<T> {
     constructor(
       public readonly queueUrl: string,
@@ -168,6 +293,9 @@ export namespace Queue {
       public readonly mapper: Mapper<T, string>
     ) {}
 
+    /**
+     * Retrieves one or more messages (up to 10), from the specified queue.
+     */
     public async receiveMessage(request?: ReceiveMessageRequest): Promise<ReceiveMessageResult<T>> {
       const response = await this.client.receiveMessage({
         ...(request || {}),
@@ -179,6 +307,9 @@ export namespace Queue {
       }));
     }
 
+    /**
+     * Delivers a message to the specified queue.
+     */
     public sendMessage(request: SendMessageRequest<T>): Promise<SendMessageResult> {
       return this.client.sendMessage({
         QueueUrl: this.queueUrl,
@@ -187,6 +318,9 @@ export namespace Queue {
       }).promise();
     }
 
+    /**
+     * Delivers a batch of messages to the specified queue.
+     */
     public sendMessageBatch(request: SendMessageBatchRequest<T>): Promise<AWS.SQS.SendMessageBatchResult> {
       return this.client.sendMessageBatch({
         QueueUrl: this.queueUrl,
