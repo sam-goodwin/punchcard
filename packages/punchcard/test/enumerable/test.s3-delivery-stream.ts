20261e890c3c36ce9afaa06ef8e07ea713de0381
Sam Goodwin <sam.goodwin1989@gmail.com>
2019-06-08 17:00:57 -0700

    Fix and improve s3 delivery stream tests.

--- packages/punchcard/test/enumerable/test.s3-delivery-stream.ts
+++ packages/punchcard/test/enumerable/test.s3-delivery-stream.ts
@@ -2,29 +2,33 @@ import 'jest';
 import sinon = require('sinon');
 
 import cdk = require('@aws-cdk/cdk');
-import { Codec, S3DeliveryStream, string } from '../../lib';
+import { Codec, S3DeliveryStream, string, struct } from '../../lib';
 import { setRuntime } from '../../lib/constants';
 import { Compression } from '../../lib/storage/glue/compression';
 
 setRuntime();
 
 describe('run', () => {
-  it('should parse event into records', async () => {
+  it('should get object amnd parse lines into records', async () => {
     const stack = new cdk.Stack(new cdk.App(), 'stack');
 
     const bucket = {
       getObject: sinon.fake.returns(Promise.resolve({
-
+        Body: `${JSON.stringify({
+          key: 'string1'
+        })}\n${JSON.stringify({
+          key: 'string2'
+        })}\n`
       }))
     };
 
     const stream = new S3DeliveryStream(stack, 'Queue', {
       codec: Codec.Json,
       compression: Compression.None,
-      type: string()
+      type: struct({key: string()})
     });
 
-    const results: string[] = [];
+    const results: Array<{key: string}> = [];
     await (stream.enumerable().forEach(stack, 'id', {
       async handle(v) {
         results.push(v);
@@ -32,9 +36,48 @@ describe('run', () => {
       }
     }).handle({
       Records: [{
-      body: JSON.stringify('string')
-    } as any]}, [bucket as any], {}));
+        s3: {
+          object: {
+            key: 'key',
+            eTag: 'eTag'
+          }
+        }
+      } as any]
+    }, [bucket as any], {}));
 
-    expect(results).toEqual(['string']);
+    expect(results).toEqual([{
+      key: 'string1'
+    }, {
+      key: 'string2'
+    }]);
+  });
+
+  it('should throw if getting object fails', async () => {
+    const stack = new cdk.Stack(new cdk.App(), 'stack');
+
+    const bucket = {
+      getObject: sinon.fake.returns(Promise.reject(new Error('fail')))
+    };
+
+    const stream = new S3DeliveryStream(stack, 'Queue', {
+      codec: Codec.Json,
+      compression: Compression.None,
+      type: struct({key: string()})
+    });
+
+    expect((stream.enumerable().forEach(stack, 'id', {
+      async handle(v) {
+        // do nothing
+      }
+    }).handle({
+      Records: [{
+        s3: {
+          object: {
+            key: 'key',
+            eTag: 'eTag'
+          }
+        }
+      } as any]
+    }, [bucket as any], {}))).rejects.toEqual(new Error('fail'));
   });
 });
