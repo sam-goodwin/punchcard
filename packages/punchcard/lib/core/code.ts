8b3be4898cde0ba34fecb5f2cd064664324bc27e
Paulo Cesar <email@pocesar.e4ward.com>
2019-12-23 19:40:21 -0300

    transpileOnly option makes ts compilation faster without type-checking

--- packages/punchcard/lib/core/code.ts
+++ packages/punchcard/lib/core/code.ts
@@ -92,7 +92,7 @@ export namespace Code {
         module: {
           rules: [
             // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-            { test: /\.tsx?$/, loader: "ts-loader" }
+            { test: /\.tsx?$/, loader: "ts-loader", options: { transpileOnly: true } }
           ]
         },
         externals,
