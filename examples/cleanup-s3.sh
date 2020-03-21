7ec33cfa2c9df5376494bfe84efd458a6d1d8250
Harry Solovay <harrysolovay@gmail.com>
2020-03-20 23:54:20 -0700

    chore: delete unnecessary files

--- examples/cleanup-s3.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-
-# deletes all non-critical S3 buckets - useful for cleaning up after testing a deployment
-
-aws s3 ls | grep -v aws-athena-query | grep -v cdktoolkit | cut -f 3 -d ' ' | while read bucket ; do aws s3 rb s3://$bucket --force ; done
